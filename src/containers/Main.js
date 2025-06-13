1d257cfb22ebcbc81d549d37bb2d5a0fcdebef20
jsonslim <deluxe87d@gmail.com>
2025-06-13 13:51:41 +0200

    router fix1

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
 import Splash from "../pages/splash/Splash";
 import Education from "../pages/education/EducationComponent";
@@ -14,10 +14,10 @@ import Error404 from "../pages/errors/error404/Error";
 export default class Main extends Component {
   render() {
     return (
-      <BrowserRouter basename="/">
+      <Router>
         <Switch>
           <Route
-            path="/"
+            path="/18watt"
             exact
             render={(props) =>
               settings.isSplash ? (
@@ -75,7 +75,7 @@ export default class Main extends Component {
             render={(props) => <Error404 {...props} theme={this.props.theme} />}
           />
         </Switch>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
